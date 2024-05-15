8d5f598815ae2c3ff52d284e7e60ac4e090f97f1
Sean Leader <spleader13@gmail.com>
2024-05-15 11:55:10 -0700

    added assumption check

--- R/proportion-test.R
+++ R/proportion-test.R
@@ -40,3 +40,37 @@ get_summary_statistics <- function(data_vec, hypo_p = -1) {
   return(summary_df)
 }
 
+
+
+
+
+
+
+#' Check the assumptions of the one sample proportion test
+#'
+#' @param data_vec A vector of TRUE/FALSE or 0/1 data representing the sample
+#'
+#' @return No return value, but prints information about the conditions
+#'
+#' @export
+check_assumptions <- function(data_vec) {
+  
+  print("Condition 1: Was the sample randomly collected from the population of interest?")
+  print(
+    str_c(
+      "Condition 2: Is the sample size (",
+      length(data_vec),
+      ") less than 10% of the population size?"
+    )
+  )
+  print(
+    str_c(
+      "Condition 3: Are both the number of positives (",
+      sum(data_vec),
+      ") and the number of negatives (",
+      length(data_vec) - sum(data_vec),
+      ") both greater than or equal to 10?"
+    )
+  )
+  
+}

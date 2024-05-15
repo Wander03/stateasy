e7aaa963995dcf41acf2a5ed9c78f721ae89c933
Wander03 <adkerr2001@gmail.com>
2024-05-15 12:19:38 -0700

    kinda progress

--- R/slr.R
+++ R/slr.R
@@ -16,28 +16,36 @@ perform_slr <- function(df, x, y, intercept = T) {
 
   predictor <- df %>% pull({{x}})
   response <- df %>% pull({{y}})  
-  
-  predictor_name <- print(deparse(substitute(x)))
-  response_name <- print(deparse(substitute(y)))
 
   stopifnot(is.numeric(predictor) & is.numeric(response))
   
+  predictor_name <- print(deparse(substitute(x)))
+  response_name <- print(deparse(substitute(y)))
+  
   model <- lm(response ~ predictor, data = df)
+  print(kable(summary(model)$coef))
   
-  print(kable(summary(m)$coef))
-  
-  create_scatter_plot(df, {{x}}, {{y}}, predictor_name, response_name)
-  
+  create_scatter_plot(df, predictor, response, predictor_name, response_name)
+  test_assumptions(model)
   
 }
 
 create_scatter_plot <- function(df, x, y, x_name, y_name) {
 
-  ggplot(df, mapping = aes({{x}}, {{y}})) +
-    geom_smooth(method = "lm", se = F, color = "red") +
+  ggplot(df, mapping = aes(x, y)) +
+    geom_point(color = "lightblue3") +
+    geom_smooth(method = "lm", formula = y ~ x, se = F, color = "red") +
     theme_bw() +
-    labs(title = paste(paste("Simple Linear Regression for", y_name, "="), expression(paste(beta, "x")))
+    labs(
+      x = x_name,
+      y = y_name
     )
+  
+}
+
+test_assumptions <- function(m) {
+  
+  plot(m)
   
 }
 
@@ -47,13 +55,13 @@ library(knitr)
 library(rlang)
 
 a <- rnorm(10)
-b <- rnorm(10)^3
+b <- a + 20 + runif(10)
 
 df <- data.frame(a, b)
 df <- rename(df, c('var1' = a, 'var2' = b))
 
-m <- perform_slr(df, var1, var2)
-m
+perform_slr(df, var1, var2)
+
 
 
 

a59685f641df396d3841ee3233bd4758e553e649
Wander03 <adkerr2001@gmail.com>
2024-05-22 11:56:09 -0700

    ctr + shft + d

--- R/slr.R
+++ R/slr.R
@@ -5,15 +5,10 @@
 #' @param y Response variable
 #' @param intercept True if the intercept should be used, False otherwise
 #'
-#' @return Common summary results from simple linear regression
+#' @return Common summary results and assumption checks for simple linear regression
 #'
-#' @importFrom dplyr select filter pull
-#' @importFrom ggplot2 ggplot aes geom_point geom_smooth theme_bw labs
-#' @importFrom knitr kable
-#' @importFrom gridExtra grid.arrange
+#' @importFrom dplyr pull
 #' @importFrom broom augment
-#' @importFrom stats lm shapiro.test cor
-#' @importFrom withr with_options
 #'
 #' @export
 perform_slr <- function(data, x, y, intercept = TRUE) {
@@ -22,18 +17,18 @@ perform_slr <- function(data, x, y, intercept = TRUE) {
   if(!is.data.frame(data)) {
     stop("Input 'data' must be a dataframe.")
   }
-  
-  if(x %in% names(data) == FALSE) {
-    stop("Column '", x, "' not found in the dataframe.")
+
+  if(deparse(substitute(x)) %in% names(data) == FALSE) {
+    stop("Column '", deparse(substitute(x)), "' not found in the dataframe.")
   }
   
-  if(y %in% names(data) == FALSE) {
-    stop("Column '", y, "' not found in the dataframe.")
+  if(deparse(substitute(y)) %in% names(data) == FALSE) {
+    stop("Column '", deparse(substitute(y)), "' not found in the dataframe.")
   }
   
   # Extract predictor and response
-  predictor <- df %>% pull({{x}})
-  response <- df %>% pull({{y}})  
+  predictor <- data %>% dplyr::pull({{x}})
+  response <- data %>% dplyr::pull({{y}})  
   
   # Check that inputs are numeric
   stopifnot(is.numeric(predictor) & is.numeric(response))
@@ -43,23 +38,40 @@ perform_slr <- function(data, x, y, intercept = TRUE) {
   if (!intercept) {
     formula <- update(formula, . ~ . - 1)
   }
-  model <- lm(formula, data = df)
-  model.diag.metrics <- augment(model)
+  model <- stats::lm(formula, data = data)
+  model.diag.metrics <- broom::augment(model)
   
   # Print summary of coefficients
   cat("Summary of coefficients:")
-  print(summary(model)$coefficients, digits = 4)
+  print(summary(model)$coefficients, digits = 3)
   
   # Create and display scatter plot with regression line
-  plot <- create_scatter_plot(df, {{x}}, {{y}}, model, model.diag.metrics, intercept)
+  plot <- create_scatter_plot(data, {{x}}, {{y}}, model, model.diag.metrics, intercept)
   print(plot)
   
   # Test assumptions
   test_assumptions(model, model.diag.metrics, intercept)
   
+  return(model)
+  
 }
 
-
+#' Creates scatter plot for simple linear regression
+#'
+#' @param df A dataframe containing the data.
+#' @param x Predictor variable
+#' @param y Response variable
+#' @param model lm object of slr model
+#' @param model.diag.metrics common diagnostic metrics for lm model
+#' @param intercept True if the intercept should be used, False otherwise
+#'
+#' @return Scatter plot for provided lm model
+#'
+#' @importFrom ggplot2 ggplot aes geom_point geom_smooth geom_segment theme_bw labs
+#' @importFrom withr with_options
+#' @importFrom plotly ggplotly
+#'
+#' @export
 create_scatter_plot <- function(df, x, y, model, model.diag.metrics, intercept) {
   
   std_residuals <- model.diag.metrics$.std.resid
@@ -76,24 +88,36 @@ create_scatter_plot <- function(df, x, y, model, model.diag.metrics, intercept) 
     y ~ x - 1
   }
   
-  p <- ggplot(data = model.diag.metrics, mapping = aes({{x}}, {{y}})) +
-    geom_smooth(method = "lm", formula = formula, se = FALSE, color = "lightblue") +
-    geom_segment(aes(xend = {{x}}, yend = .fitted), color = "red", size = 0.3) +
-    geom_point(aes(
+  p <- ggplot2::ggplot(data = model.diag.metrics, mapping = ggplot2::aes({{x}}, {{y}})) +
+    ggplot2::geom_smooth(method = "lm", formula = formula, se = FALSE, color = "lightblue") +
+    ggplot2::geom_segment(ggplot2::aes(xend = {{x}}, yend = .fitted), color = "red", size = 0.3) +
+    ggplot2::geom_point(ggplot2::aes(
       text = 
     ),
     color = "black"
     ) +
-    theme_bw()
+    ggplot2::theme_bw()
   
-  with_options(
-    list(digits = 4),
-    ggplotly(p, tooltip = c("x", "y", ".fitted"), width = 800, height = 600)
+  withr::with_options(
+    list(digits = 3),
+    plotly::ggplotly(p, tooltip = c("x", "y", ".fitted"), width = 800, height = 600)
   )
   
 }
 
-
+#' Checks assumptions for simple linear regression
+#'
+#' @param model lm object of slr model
+#' @param model.diag.metrics common diagnostic metrics for lm model
+#' @param intercept True if the intercept should be used, False otherwise
+#'
+#' @return Plots of assumptions for provided lm model
+#'
+#' @importFrom ggplot2 ggplot aes geom_point geom_smooth geom_segment stat_qq stat_qq_line geom_hline geom_vline theme_bw labs
+#' @importFrom gridExtra grid.arrange
+#' @importFrom stats lm shapiro.test cor
+#'
+#' @export
 test_assumptions <- function(model, model.diag.metrics, intercept) {
   
   # Extract residuals, fitted values, leverage, standardized residuals and cooks distance
@@ -106,61 +130,61 @@ test_assumptions <- function(model, model.diag.metrics, intercept) {
   violated_indices <- list()
   
   # Linearity
-  plot1 <- ggplot(data = NULL, aes(x = fitted, y = residuals)) +
-    geom_point() +
-    geom_smooth(method = "loess",  formula = y ~ x, se = FALSE, color = "red") +
-    theme_bw() +
-    labs(
+  plot1 <- ggplot2::ggplot(data = NULL, ggplot2::aes(x = fitted, y = residuals)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_smooth(method = "loess",  formula = y ~ x, se = FALSE, color = "red") +
+    ggplot2::theme_bw() +
+    ggplot2::labs(
       x = "Fitted values",
       y = "Residuals",
       title = "Residuals vs Fitted"
     )
   
-  linearity_test <- lm(residuals ~ fitted)
+  linearity_test <- stats::lm(residuals ~ fitted)
   if (summary(linearity_test)$coefficients[2, 4] < 0.05) {
     violated_indices[["linearity"]] <- which(summary(linearity_test)$coefficients[, 4] < 0.05)
   }
   
   # Normality
-  plot2 <- ggplot(data = NULL, aes(sample = residuals)) +
-    stat_qq() +
-    stat_qq_line(color = "red") +
-    theme_bw() +
-    labs(
+  plot2 <- ggplot2::ggplot(data = NULL, ggplot2::aes(sample = residuals)) +
+    ggplot2::stat_qq() +
+    ggplot2::stat_qq_line(color = "red") +
+    ggplot2::theme_bw() +
+    ggplot2::labs(
       title = "Normal Q-Q Plot"
     )
   
-  shapiro_test <- shapiro.test(residuals)
+  shapiro_test <- stats::shapiro.test(residuals)
   if (shapiro_test$p.value < 0.05) {
     violated_indices[["normality"]] <- TRUE
   }
   
   # Homoscedasticity
-  plot3 <- ggplot(data = NULL, aes(x = fitted, y = abs(residuals))) +
-    geom_point() +
-    geom_smooth(method = "loess", formula = y ~ x, se = FALSE, color = "red") +
-    theme_bw() +
-    labs(
+  plot3 <- ggplot2::ggplot(data = NULL, ggplot2::aes(x = fitted, y = abs(residuals))) +
+    ggplot2::geom_point() +
+    ggplot2::geom_smooth(method = "loess", formula = y ~ x, se = FALSE, color = "red") +
+    ggplot2::theme_bw() +
+    ggplot2::labs(
       x = "Fitted values",
       y = "Absolute Residuals",
       title = "Scale-Location Plot"
     )
   
-  if (cor(fitted, abs(residuals)) > 0.5) {
+  if (stats::cor(fitted, abs(residuals)) > 0.5) {
     violated_indices[["homoscedasticity"]] <- TRUE
   }
   
   # Leverage
   leverage_threshold <- 2 * length(coef(model)) / nrow(model.diag.metrics)
   
-  plot4 <- ggplot(data = model.diag.metrics, aes(x = .hat, y = .std.resid)) +
-    geom_point() +
-    geom_hline(yintercept = 3, linetype = "dashed", color = "red") +
-    geom_hline(yintercept = -3, linetype = "dashed", color = "red") +
-    geom_vline(xintercept = 2 * length(coef(model)) / nrow(model.diag.metrics), linetype = "dashed", color = "blue") +
-    geom_smooth(method = "loess", formula = y ~ x, se = FALSE, color = "lightblue") +
-    theme_bw() +
-    labs(
+  plot4 <- ggplot2::ggplot(data = model.diag.metrics, ggplot2::aes(x = .hat, y = .std.resid)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_hline(yintercept = 3, linetype = "dashed", color = "red") +
+    ggplot2::geom_hline(yintercept = -3, linetype = "dashed", color = "red") +
+    ggplot2::geom_vline(xintercept = 2 * length(coef(model)) / nrow(model.diag.metrics), linetype = "dashed", color = "blue") +
+    ggplot2::geom_smooth(method = "loess", formula = y ~ x, se = FALSE, color = "lightblue") +
+    ggplot2::theme_bw() +
+    ggplot2::labs(
       x = "Leverage",
       y = "Standardized Residuals",
       title = "Residuals vs Leverage"
@@ -181,12 +205,12 @@ test_assumptions <- function(model, model.diag.metrics, intercept) {
   # Cooks Dist.
   cooks_d_threshold <- qf(.5, length(coef(model)), nrow(model.diag.metrics) - length(coef(model)))
   
-  plot5 <- ggplot(data = model.diag.metrics, aes_string(x = ".hat", y = "cooks_d")) +
-    geom_point() +
-    geom_hline(yintercept = cooks_d_threshold, linetype = "dashed", color = "red") +
-    geom_smooth(method = "loess", formula = y ~ x, se = FALSE, color = "lightblue") +
-    theme_bw() +
-    labs(
+  plot5 <- ggplot2::ggplot(data = model.diag.metrics, ggplot2::aes_string(x = ".hat", y = "cooks_d")) +
+    ggplot2::geom_point() +
+    ggplot2::geom_hline(yintercept = cooks_d_threshold, linetype = "dashed", color = "red") +
+    ggplot2::geom_smooth(method = "loess", formula = y ~ x, se = FALSE, color = "lightblue") +
+    ggplot2::theme_bw() +
+    ggplot2::labs(
       x = "Leverage",
       y = "Cook's Distance",
       title = "Cook's Distance vs Leverage"
@@ -207,7 +231,7 @@ test_assumptions <- function(model, model.diag.metrics, intercept) {
   }
   
   # Display plots
-  grid.arrange(plot1, plot2, plot3, plot4, plot5, ncol = 2)
+  gridExtra::grid.arrange(plot1, plot2, plot3, plot4, plot5, ncol = 2)
   
 }
 # 
@@ -222,12 +246,12 @@ test_assumptions <- function(model, model.diag.metrics, intercept) {
 # 
 # a <- rnorm(10)
 # b <- a + rnorm(10)
-# 
+# # 
 # df <- data.frame(a, b)
 # df <- rename(df, c('var1' = a, 'var2' = b))
-# 
-# df <- rbind(df, data.frame('var1' = 1, 'var2' = 100))
-# 
+# # 
+# # df <- rbind(df, data.frame('var1' = 1, 'var2' = 100))
+# # 
 # perform_slr(df, var1, var2, TRUE)
 
 

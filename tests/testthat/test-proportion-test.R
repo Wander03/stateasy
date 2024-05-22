86966bc5fc4470b2e993e4bbd6525026cdaf5444
Sean Leader <spleader13@gmail.com>
2024-05-22 11:46:54 -0700

    add test case for assumption check

--- tests/testthat/test-proportion-test.R
+++ tests/testthat/test-proportion-test.R
@@ -32,33 +32,55 @@ test_that("Check data vector type with binary numeric data", {
   expect_equal(my_result, correct_result)
 })
 
-test_that("Check that hypothesis direction check is functional", {
+test_that("Hypothesis direction check: incorrect specification", {
   
   correct_result <- FALSE
-  
   
   my_result <- valid_direction(">=")
   
   expect_equal(my_result, correct_result)
 })
 
+test_that("Hypothesis direction check: correct specification", {
+  
+  correct_result <- TRUE
+  
+  my_result <- valid_direction("!=")
+  
+  expect_equal(my_result, correct_result)
+})
 
 
+test_that("Validate that assumption checking is working properly", {
+  
+  correct_result <- data.frame(
+    condition = c("Random", "Independence", "Normality"),
+    description = c(
+      "Was the sample randomly collected from the population of interest?",
+      "Is the sample size (50) less than 10% of the population size?",
+      "Are both the number of positives (20) and the number of negatives (30) both greater than or equal to 10?"
+      )
+    )
+  
+  my_result <- check_assumptions(c(rep(1, 20), rep(0, 30)))
+  
+  expect_equal(my_result, correct_result)
+})
 
-# 
-# test_that("add_commentary compliments people with lots of grams", {
-#   correct_result <- "You go, Glen Coco!"
-#   
-#   my_result <- add_commentary("Glen Coco", 4)
-#   
-#   expect_equal(my_result, correct_result)
-# })
-# 
-# 
-# test_that("add_commentary compliments people with lots of grams", {
-#   correct_result <- "You go, Glen Coco!"
-#   
-#   my_result <- add_commentary("Glen Coco", 4)
-#   
-#   expect_equal(my_result, correct_result)
-# })
+
+test_that("Confidence Interval correctly calculated with no hypothesis test", {
+  
+  correct_result <- data.frame(
+    condition = c("Random", "Independence", "Normality"),
+    description = c(
+      "Was the sample randomly collected from the population of interest?",
+      "Is the sample size (50) less than 10% of the population size?",
+      "Are both the number of positives (20) and the number of negatives (30) both greater than or equal to 10?"
+    )
+  )
+  
+  my_result <- check_assumptions(c(rep(1, 20), rep(0, 30)))
+  
+  expect_equal(my_result, correct_result)
+})
+
